d66103fa884542e7cfbf9b004271755d37ac7782
Samuel Young <samueldy@umich.edu>
2023-02-15 19:10:13 -0500

    Have forced renewal so we can update every 60 days instead of 90 days

--- scripts/renew.sh
+++ scripts/renew.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-certbot renew --deploy-hook /scripts/update_rancher.sh
+# Specify forced renewal so that we can let the cron job take care of
+# auto-renewing before the certificate is expired. Like every 60 days.
+certbot renew --force-renewal --deploy-hook /scripts/update_rancher.sh
