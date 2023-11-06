cae90c2e0a82830aaab9573f81bd5230242c980a
Samuel Young <samueldy@umich.edu>
2023-11-06 14:51:52 -0500

    Start trying to handle cert updates with anacron+cronie in case of surprise NERSC power outage

--- scripts/renew.sh
+++ scripts/renew.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+LOGSDIR=/var/spool/anacron-store/logs
 
 # Specify forced renewal so that we can let the cron job take care of
 # auto-renewing before the certificate is expired. Like every 60 days.
-certbot renew --force-renewal --deploy-hook /scripts/update_rancher.sh
+certbot renew --force-renewal --deploy-hook /scripts/update_rancher.sh &&
+    echo "TLS certs renewed as of $(date -Is)." 2>&1 | tee -a $LOGSDIR/anacrontab-output.log
--- scripts/start-crond.sh
+++ scripts/start-crond.sh
@@ -3,5 +3,11 @@
 # Need to wait for cron volume mount to become available
 sleep 10
 
-# Entrypoint to run cron daemon instead of the certbot CLi
-/usr/sbin/crond -f -l 2 -L /dev/stdout 2>/dev/stderr
+# Make necessary anacron timestamp and log directories if they don't already
+# exist.
+mkdir -p /var/spool/anacron-store/anacron
+mkdir -p /var/spool/anacron-store/logs
+
+# Entrypoint to run cron daemon instead of the certbot CLI. Run in foreground
+# so container doesn't error out.
+/usr/sbin/crond -n >/dev/stdout 2>/dev/stderr
